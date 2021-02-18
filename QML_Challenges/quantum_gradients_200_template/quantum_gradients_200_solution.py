45ef994396a4f770a6672ad57b090f9d1ba8a781
mathemage <mathemage@gmail.com>
2021-02-18 17:41:33 +0100

    Solve quantum_gradients_200

--- QML_Challenges/quantum_gradients_200_template/quantum_gradients_200_solution.py
+++ QML_Challenges/quantum_gradients_200_template/quantum_gradients_200_solution.py
@@ -46,33 +46,36 @@ def gradient_200(weights, dev):
     hessian = np.zeros([5, 5], dtype=np.float64)
 
     # QHACK #
-    one_hot_1 = np.zeros_like(gradient)
-    one_hot_2 = np.zeros_like(hessian)
+    one_hot = np.zeros_like(gradient)
     memo = {}
     s = 1.0
 
     s *= 2
     denominator = 2 * np.sin(s)
-    for wi in range(len(weights)):
-        one_hot_1[wi] = s
+    for i in range(len(weights)):
+        one_hot[i] = s
 
-        mi = wi + 1
-        memo[mi] = circuit(weights + one_hot_1)
-        memo[-mi] = circuit(weights - one_hot_1)
+        mi = i + 1
+        memo[mi] = circuit(weights + one_hot)
+        memo[-mi] = circuit(weights - one_hot)
 
-        gradient[wi] = (memo[mi] - memo[-mi]) / denominator
-        one_hot_1[wi] = 0
+        gradient[i] = (memo[mi] - memo[-mi]) / denominator
+        one_hot[i] = 0
 
     s /= 2
     denominator = 2 * np.sin(s)
-    # off-diagonal TODO
-    # for wi in range(len(weights)):
-    #     for wj in range(wi + 1, len(weights)):
-    #         # print(f"wi == {wi}, wi == {wj}")
-    #         one_hot_2[wi] = one_hot_2[wj] = s
-    #         hessian[wi, wj] = (circuit(weights + one_hot_2) - circuit(weights - one_hot_2)) / denominator
-    #         hessian[wj, wi] = hessian[wi, wj]
-    #         one_hot_2[wi] = one_hot_2[wj] = 0
+    # off-diagonal
+    for i in range(len(weights)):
+        for j in range(i + 1, len(weights)):
+            # print(f"i == {i}, i == {j}")
+            for sign_j in [1, -1]:
+                for sign_i in [1, -1]:
+                    one_hot = np.zeros_like(gradient)
+                    one_hot[i] = sign_i * s
+                    one_hot[j] = sign_j * s
+                    hessian[i, j] += sign_i * sign_j * circuit(weights + one_hot)
+            hessian[i, j] /= denominator ** 2
+            hessian[j, i] = hessian[i, j]
 
     # diagonal
     f_n0 = circuit(weights)
@@ -80,12 +83,13 @@ def gradient_200(weights, dev):
         mii = wii + 1
         hessian[wii, wii] = (memo[mii] - f_n0 - (f_n0 - memo[-mii])) / denominator ** 2
 
-    print(gradient)
-    print()
-    print(hessian)
-    print()
-    print(dev.num_executions)
-    print()
+    # np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
+    # print(gradient)
+    # print()
+    # print(hessian)
+    # print()
+    # print(dev.num_executions)
+    # print()
     # QHACK #
 
     return gradient, hessian, circuit.diff_options["method"]
